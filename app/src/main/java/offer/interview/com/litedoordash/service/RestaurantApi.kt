a6884dda1fc2d198e785dbf29c59256348f12834
jundong <jdongsf@gmail.com>
2019-09-07 21:43:00 -0700

    use constant to replace hard code

--- app/src/main/java/offer/interview/com/litedoordash/service/RestaurantApi.kt
+++ app/src/main/java/offer/interview/com/litedoordash/service/RestaurantApi.kt
@@ -1,14 +1,17 @@
 package offer.interview.com.litedoordash.service
 
 import offer.interview.com.litedoordash.data.Restaurant
+import offer.interview.com.litedoordash.util.AppConstants.LAT
+import offer.interview.com.litedoordash.util.AppConstants.LIMIT
+import offer.interview.com.litedoordash.util.AppConstants.LNG
 import retrofit2.http.GET
 import retrofit2.http.Query
 
 interface RestaurantApi {
 
     @GET("restaurant/")
-    suspend fun getRestaurants(@Query("lat") lat: String = "37.422740",
-                       @Query("lng") lng: String = "-122.139956",
+    suspend fun getRestaurants(@Query("lat") lat: String = LAT,
+                       @Query("lng") lng: String = LNG,
                        @Query("offset") offset: Int = 0,
-                       @Query("limit") limit: Int = 50): List<Restaurant>
+                       @Query("limit") limit: Int = LIMIT): List<Restaurant>
 }
